1a18f16680eed6fd7dbd68079983e8a04ec7fde9
bigperson <daein7076@gmail.com>
2024-04-01 19:13:30 +0900

    7.2 Data Flow

--- Home.py
+++ Home.py
@@ -1,11 +1,23 @@
 import streamlit as st
+from datetime import datetime
 
-st.title("Hello world!")
+#! data 에 관련된 것은 바뀔 때마다 페이지 전체가 refresh 된다.
 
-st.subheader("Welcome to Streamlit")
+today = datetime.today().strftime("%H:%M:%S")
 
-st.markdown(
-    """
-#### I love it!
-"""
-)
+st.title(today)
+
+model = st.selectbox("Choose your model", ("GPT-3", "GPT-4"))
+
+if model == "GPT-3":
+    st.write("cheap")
+else:
+    st.write("expensive")
+
+name = st.text_input("What is your name?")
+
+st.write(name)
+
+value = st.slider("temperature", min_value=0.1, max_value=1.0)
+
+st.write(value)
