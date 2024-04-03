bb1d19c94cac1407c31aea7376f7be6609f8933f
bigperson <daein7076@gmail.com>
2024-04-03 19:11:48 +0900

    7.8 Chain

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -4,11 +4,30 @@ from langchain.document_loaders import UnstructuredFileLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
 from langchain.storage import LocalFileStore
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
 
 
 st.set_page_config(page_icon="📃", page_title="DocumentGPT")
 st.title("DocumentGPT")
+
+llm = ChatOpenAI(temperature=0.1)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+Answer the question using ONLY the following context. If you don't know the answer just say you don't know. DON'T make anything up.
+     
+     Context: {context}
+""",
+        ),
+        ("human", "{question}"),
+    ]
+)
 
 
 @st.cache_data(show_spinner="Embedding file...")
@@ -52,6 +71,10 @@ def paint_history():
         )
 
 
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+
 st.markdown(
     """
 WELCOME
@@ -71,5 +94,16 @@ if file:
     message = st.chat_input("Ask anything about your file...")
     if message:
         send_message(message, "human")
+        chain = (
+            {
+                "context": retriver | RunnableLambda(format_docs),
+                "question": RunnablePassthrough(),
+            }
+            | prompt
+            | llm
+        )
+        response = chain.invoke(message)
+        send_message(response.content, "ai")
+
 else:
     st.session_state["messages"] = []
