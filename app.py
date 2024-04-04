4504fd135a7efbdb26d9e989f1679b3e8e2e9c77
bigperson <daein7076@gmail.com>
2024-04-05 00:50:56 +0900

    code challenge | document gpt ver1.7

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ class ChatCallbackHandler(BaseCallbackHandler):
 def embed_file(file):
 
     file_content = file.read()
-    file_path = f"./..cache/files/{file.name}"
+    file_path = f"./.cache/files/{file.name}"
 
     with open(file_path, "wb") as f:
         f.write(file_content)
