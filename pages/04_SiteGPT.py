f11f64935d92c197c7fe6d8d9430219f87bfd8a1
bigperson <daein7076@gmail.com>
2024-04-08 23:25:50 +0900

    10.4 Map Re Rank Chain Part.1

--- pages/04_SiteGPT.py
+++ pages/04_SiteGPT.py
@@ -1,6 +1,56 @@
 from langchain.document_loaders import SitemapLoader
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
 import streamlit as st
+
+llm = ChatOpenAI(
+    temperature=0.1,
+)
+
+answers_prompt = ChatPromptTemplate.from_template(
+    """
+    Using ONLY the following context answer the user's question. If you can't just say you don't know, don't make anything up.
+                                                  
+    Then, give a score to the answer between 0 and 5.
+
+    If the answer answers the user question the score should be high, else it should be low.
+
+    Make sure to always include the answer's score even if it's 0.
+
+    Context: {context}
+                                                  
+    Examples:
+                                                  
+    Question: How far away is the moon?
+    Answer: The moon is 384,400 km away.
+    Score: 5
+                                                  
+    Question: How far away is the sun?
+    Answer: I don't know
+    Score: 0
+                                                  
+    Your turn!
+
+    Question: {question}
+"""
+)
+
+
+def get_answers(inputs):
+    docs = inputs["docs"]
+    question = inputs["question"]
+    answers_chain = answers_prompt | llm
+    answers = []
+    for doc in docs:
+        result = answers_chain.invoke(
+            {"question": question, "context": doc.page_content}
+        )
+        answers.append(result.content)
+    st.write(answers)
 
 
 def parse_page(soup):
@@ -15,27 +65,29 @@ def parse_page(soup):
         .replace("\n", " ")
         .replace("\xa0", " ")
         .replace("CloseSearch Submit Blog", "")
-        .replace("View all articles ", "")
     )
 
 
 @st.cache_data(show_spinner="Loading website...")
 def load_website(url):
     splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
-        chunk_size=1000, chunk_overlap=200
+        chunk_size=1000,
+        chunk_overlap=200,
     )
     loader = SitemapLoader(
         # filter_urls를 통해 원하는 site만 docs에 넣을 수 있음
         # 정규표현식을 이용해서 원하는 url만 가져올 수 있음.
         # ex) ^(.*\/blog\/).* 는 /blog/ 가 url에 존재하면 docs에 추가 |  ?! 는 반대의 의미 (부정)
         url,
-        filter_urls=[r"^(.*\/blog\/).*"],
         parsing_function=parse_page,
+        # filter_urls=[r"^(.*\/blog\/).*"],
+        # filter_urls=["https://openai.com/blog/data-partnerships"],
     )
     # request 횟수를 조정해(느리게) 웹사이트에서 차단당하는 것을 막을 수 있음.
-    # loader.requests_per_second = 1
+    loader.requests_per_second = 2
     docs = loader.load_and_split(text_splitter=splitter)
-    return docs
+    vector_store = FAISS.from_documents(docs, OpenAIEmbeddings())
+    return vector_store.as_retriever()
 
 
 st.set_page_config(
@@ -60,7 +112,13 @@ with st.sidebar:
 if url:
     if ".xml" not in url:
         with st.sidebar:
-            st.error("Please write down a Sitemap URL")
+            st.error("Please write down a Sitemap URL.")
     else:
-        docs = load_website(url)
-        st.write(docs)
+        retriever = load_website(url)
+
+        chain = {
+            "docs": retriever,
+            "question": RunnablePassthrough(),
+        } | RunnableLambda(get_answers)
+
+        chain.invoke("What is the pricing of GPT-4 Turbo with vision.")
